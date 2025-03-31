7f394a627e24439fd804ce436d6e355a6ae35c5d
EthanWZech <ethanwzech@gmail.com>
2025-03-31 16:33:42 -0500

    Config Tailwind

--- React/loom/src/Example.tsx
+++ React/loom/src/Example.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import './Example.css'
 
 export const Example = () => {
   const thingamajig = (num: number)=> {
@@ -8,7 +9,7 @@ export const Example = () => {
 
   return (
     <div>
-        <h1>Hello</h1>
+        <h1 className='text-blue-500'>Hello</h1>
         <div>{thingamajig(3)}</div>
     </div>
   )

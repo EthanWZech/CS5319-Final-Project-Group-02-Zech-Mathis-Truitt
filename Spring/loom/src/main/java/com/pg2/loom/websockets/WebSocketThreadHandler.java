b85285178a64639d2c38870b50c9362441c0b255
EthanWZech <ethanwzech@gmail.com>
2025-04-10 16:02:36 -0500

    Web socket sending Json objects

--- Spring/loom/src/main/java/com/pg2/loom/websockets/WebSocketThreadHandler.java
+++ Spring/loom/src/main/java/com/pg2/loom/websockets/WebSocketThreadHandler.java
@@ -1,5 +1,7 @@
 package com.pg2.loom.websockets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pg2.loom.message.TestMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -10,6 +12,7 @@ import java.util.concurrent.CopyOnWriteArraySet;
 
 public class WebSocketThreadHandler extends TextWebSocketHandler {
     private static final ConcurrentHashMap<String, CopyOnWriteArraySet<WebSocketSession>> threads = new ConcurrentHashMap<>();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
@@ -17,16 +20,19 @@ public class WebSocketThreadHandler extends TextWebSocketHandler {
         threads.putIfAbsent(threadId, new CopyOnWriteArraySet<>());
         threads.get(threadId).add(session);
 
-        session.sendMessage(new TextMessage("Connected to thread: " + threadId));
+        TestMessage testMessage = new TestMessage("System", "Connected to thread: " + threadId);
+        session.sendMessage(new TextMessage(mapper.writeValueAsString(testMessage)));
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String threadId = extractThreadId(session);
 
+        TestMessage testMessage = mapper.readValue(message.getPayload(), TestMessage.class);
+
         for (WebSocketSession client : threads.getOrDefault(threadId, new CopyOnWriteArraySet<>())) {
             if(client.isOpen()) {
-                client.sendMessage(new TextMessage(message.getPayload()));
+                client.sendMessage(new TextMessage(mapper.writeValueAsString(message)));
             }
         }
     }
